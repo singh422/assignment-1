ab0c94896fae1adafdc45e8197d195b548d5513d
Avinash <@purdue.edu>
2017-05-30 19:15:34 +0530

    Initial Commit

---
+++ assignment 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment 1
+//
+//  Created by Avinash Singh on 30/05/17.
+//  Copyright © 2017 Avinash Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
