f31a26ec4a457704bcb538f4d5b2b99b3b384b7c
Avinash <@purdue.edu>
2017-05-31 16:45:00 +0530

    first commit

---
+++ assignment 1/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  assignment 1
+//
+//  Created by Avinash Singh on 30/05/17.
+//  Copyright © 2017 Avinash Singh. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
